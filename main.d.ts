8e8b1a596148675645de6f8a952639fda4ccd2a9
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-07-05 19:49:28 -0300

    Update version to 13.1.2 on main.d.ts

--- main.d.ts
+++ main.d.ts
@@ -1,4 +1,4 @@
-// Type definitions for byte-data 13.1.1
+// Type definitions for byte-data 13.1.2
 // Project: https://github.com/rochars/byte-data
 // Definitions by: Rafael S. Rocha <https://github.com/rochars>
 // Definitions: https://github.com/rochars/byte-data
