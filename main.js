73eabf3535e380eff36bb903ebd84749b07a3204
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-07-08 02:50:56 -0300

    Try 64-bit float read/write in BE hosts

--- main.js
+++ main.js
@@ -269,7 +269,7 @@ export function unpackArrayTo(buffer, theType, output, index=0, end=null) {
   }
 }
 
-// Do not allow big-endian envs
-if (new Uint8Array(new Uint32Array([0x12345678]).buffer)[0] === 0x12) {
-  throw new Error('This library is for little-endian environments only.');
-}
+// Issue warning if running on big-endian env
+//if (new Uint8Array(new Uint32Array([0x12345678]).buffer)[0] === 0x12) {
+//  throw new Error('This library is for little-endian environments only.');
+//}
