043d26faca78cd187cc29168e10117ac489b7a3c
Rafael Rocha <rocha.rafaelsilva@gmail.com>
2018-07-13 15:20:02 -0300

    Try fix test/dist/error.js for Node@10

--- test/dist/error.js
+++ test/dist/error.js
@@ -9,8 +9,6 @@
 
 var byteData = byteData || require('../../test/loader.js');
 var testFunc;
-var typeError = "Bad type definition.";
-var floatTypeError = "Bad float type.";
 var assert = assert || require('assert');
 
 describe('Errors', function() {
@@ -53,24 +51,24 @@ describe('Errors', function() {
         testFunc = function() {
             byteData.pack(2, {"bits": 65});
         };
-        assert.throws(testFunc, typeError);
+        assert.throws(testFunc, /Bad type definition./);
     });
     it("Less than 1 bit (0)", function () {
         testFunc = function() {
             byteData.pack(2, {"bits": 0});
         };
-        assert.throws(testFunc, typeError);
+        assert.throws(testFunc, /Bad type definition./);
     });
     it("Less than 1 bit (-1)", function () {
         testFunc = function() {
             byteData.pack(2, {"bits": -1});
         };
-        assert.throws(testFunc, typeError);
+        assert.throws(testFunc, /Bad type definition./);
     });
     it("17 float (-1)", function () {
         testFunc = function() {
             byteData.pack(2, {"bits": 17, "float": true});
         };
-        assert.throws(testFunc, floatTypeError);
+        assert.throws(testFunc, /Bad float type./);
     });
 });
